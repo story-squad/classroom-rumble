d55d5fb7e41434687841db00b19f5a1cd2aea40d
Brandon Ramirez <bran.ramirez.don@gmail.com>
2021-02-17 10:49:44 -0800

    Finished auth page layout and api call

---
+++ src/components/pages/AuthPage/AuthPageContainer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import RenderAuthPage from './RenderAuthPage';
+
+const AuthPageContainer = (): React.ReactElement => {
+  return <RenderAuthPage />;
+};
+
+export default AuthPageContainer;
