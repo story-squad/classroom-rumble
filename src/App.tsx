ed551ca806bf81c1f795472eada4a207db1971f0
Brandon Ramirez <bran.ramirez.don@gmail.com>
2021-02-21 00:43:53 -0800

    Refactoring for manual rostering.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { AuthPage } from './components/pages/AuthPage';
+import { CleverPage } from './components/pages/CleverPage';
 
 const App = (): React.ReactElement => {
   return (
@@ -9,6 +10,10 @@ const App = (): React.ReactElement => {
         {/* Public Routes */}
         <Route path="/login" component={AuthPage} />
         {/* Private Routes */}
+
+        {/* Clever Routes */}
+        <Route path="/oauth/clever" component={CleverPage} />
+
         {/* Fallback redirect to main app homepage! Change the 'to' property after setting up routes. */}
         <Route path="/" component={() => <Redirect to="/homepage" />} />
       </Switch>
