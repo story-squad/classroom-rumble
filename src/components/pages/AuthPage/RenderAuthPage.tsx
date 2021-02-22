3e26fe55e182fa127ea3223c15ee6f7f5c45c2d2
Brandon Ramirez <bran.ramirez.don@gmail.com>
2021-02-22 09:49:38 -0800

    Changed auth form state to use recoil.

--- src/components/pages/AuthPage/RenderAuthPage.tsx
+++ src/components/pages/AuthPage/RenderAuthPage.tsx
@@ -1,20 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { auth } from '../../../state';
 import AuthHeader from './AuthForms/AuthHeader';
 import LoginForm from './AuthForms/LoginForm';
 import SignUpForm from './AuthForms/SignUpForm';
 
 const RenderAuthPage = (): React.ReactElement => {
-  const [isLogin, setIsLogin] = useState(true);
-
+  const isLogin = useRecoilValue(auth.form.isLogin);
   return (
     <div className="auth-page-wrapper">
       <div className="auth-page">
-        <AuthHeader isLogin={isLogin} setIsLogin={setIsLogin} />
-        {isLogin ? (
-          <LoginForm />
-        ) : (
-          <SignUpForm isLogin={isLogin} setIsLogin={setIsLogin} />
-        )}
+        <AuthHeader />
+        {isLogin ? <LoginForm /> : <SignUpForm />}
       </div>
     </div>
   );
