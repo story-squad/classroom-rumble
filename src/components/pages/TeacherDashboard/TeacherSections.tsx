ebc5b20b8a522dca00a78380d4823d49548d7e03
Steven Sussman <sjsussman@gmail.com>
2021-02-22 18:32:02 -0500

    created api call for teacher sections

---
+++ src/components/pages/TeacherDashboard/TeacherSections.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { Sections } from '../../../api';
+import { ISection } from '../../../api/Sections';
+
+const TeacherSections = (): React.ReactElement => {
+  const [teacherSections, setTeacherSections] = useState<
+    undefined | ISection[]
+  >();
+
+  useEffect(() => {
+    Sections.getSections(1)
+      .then((res) => {
+        setTeacherSections(res);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div className="sectionInfo">
+      {teacherSections?.map((data, i) => (
+        <div key={i}>
+          <div>{data.name}</div>
+          <div>{data.active}</div>
+          <div>{data.grade}</div>
+          <div>{data.subject}</div>
+          <div>{data.joinCode}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TeacherSections;
