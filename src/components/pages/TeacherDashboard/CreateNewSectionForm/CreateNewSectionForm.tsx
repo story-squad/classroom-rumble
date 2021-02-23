1e1ee618e043c134655896494db357baeb75a1e5
victor dronov <victordronov1@gmail.com>
2021-02-23 15:29:24 -0500

    taking in fake data diplaying in inputs

--- src/components/pages/TeacherDashboard/CreateNewSectionForm/CreateNewSectionForm.tsx
+++ src/components/pages/TeacherDashboard/CreateNewSectionForm/CreateNewSectionForm.tsx
@@ -1,32 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { INewSectionBody } from '../../../../api/Section';
+import { Section } from '../../../../api';
+import { IData, INewSectionBody } from '../../../../api/CreateSection';
 import { Input } from '../../../common';
-
-const TestData = [
-  // subjectId: string // We'll get these from the backend
-  // gradeId: string // Getting these from the backend as well!
-  { name: 'Science', subjectId: '4', gradeId: 'Item 4' },
-  { name: 'Math', subjectId: '4', gradeId: 4 },
-  { name: 'Art', subjectId: '4', gradeId: 4 },
-  { name: 'Reading', subjectId: '4', gradeId: 4 },
-  { name: 'Math>', subjectId: '2', gradeId: 2 },
-  { name: 'Reading', subjectId: '6', gradeId: 2 },
-  { name: 'Art', subjectId: '4', gradeId: 2 },
-  { name: 'Art', subjectId: '3', gradeId: 2 },
-];
 
 const NewSection = (): React.ReactElement => {
   const { errors, register, handleSubmit } = useForm();
+  const [options, setOptions] = useState<IData[]>();
 
-  const onSubmit: SubmitHandler<INewSectionBody> = async (
-    data,
-  ): Promise<void> => {
+  const onSubmit: SubmitHandler<INewSectionBody> = async (): // data,
+  Promise<void> => {
     try {
     } catch (err) {
       console.log(err);
     }
   };
+
+  useEffect(() => {
+    Section.getSectionData()
+      .then((res) => {
+        setOptions(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -39,13 +37,29 @@ const NewSection = (): React.ReactElement => {
         placeholder="Enter class name"
         rules={{ required: 'Classname is required!' }}
       />
-      <select name="Test" value="Test">
-        {/* <option value="Grade">Items</option> */}
-        {/* The value being put in the potions needs  */}
-        {TestData.map((item, gradeId) => (
-          <option key={gradeId}>{item.gradeId}</option>
-        ))}
-      </select>
+      {/* <option value="Grade">Items</option> */}
+      {/* The value being put in the potions needs  */}
+      {options?.map(({ subjects, grades }, i) => (
+        <>
+          <label>
+            Grade:
+            <select>
+              {grades.map(({ gradeId, value }) => (
+                <option key={gradeId}>{value}</option>
+              ))}
+            </select>
+          </label>
+          <label>
+            Subject:
+            <select key={i} name="Test" value="Test">
+              {subjects.map(({ subjectId, value }) => (
+                <option key={subjectId}>{value}</option>
+              ))}
+            </select>
+          </label>
+        </>
+      ))}
+      ,
     </form>
   );
 };
