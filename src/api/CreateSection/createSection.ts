28ab5c245972f20af5ab6dab43cb3545d6ec2d7f
victor dronov <victordronov1@gmail.com>
2021-02-23 15:29:24 -0500

    taking in fake data diplaying in inputs

---
+++ src/api/CreateSection/createSection.ts
@@ -0,0 +1,69 @@
+import { axiosWithAuth } from '../axiosWithConfig';
+
+export interface INewSectionBody {
+  name: string; // The name of the class!
+  subjectId: string; // We'll get these from the backend
+  gradeId: string; // Getting these from the backend as well!
+}
+
+export interface IFullSection extends INewSectionBody {
+  joinCode: string;
+  id: number;
+  active: boolean;
+}
+
+export const CreateNewSection = async (
+  body: INewSectionBody,
+  teacherId: number,
+): Promise<IFullSection> => {
+  const { data } = await axiosWithAuth().post(
+    `/api/rumble/teachers/${teacherId}/sections`,
+    body,
+  );
+  return data;
+};
+
+// Endpoint: POST /api/rumble/teachers/:teacherId/sections
+
+export interface IGrade {
+  gradeId: string;
+  grade: number;
+}
+
+export interface ISubject {
+  subjectId: string;
+  subject: string;
+}
+
+const dummyData = [
+  {
+    grades: [
+      { gradeId: '0', value: '3' },
+      { gradeId: '1', value: '4' },
+      { gradeId: '2', value: '5' },
+      { gradeId: '3', value: '6' },
+    ],
+    subjects: [
+      { subjectId: '3', value: 'Math' },
+      { subjectId: '4', value: 'Art' },
+      { subjectId: '5', value: 'English' },
+      { subjectId: '6', value: 'Science' },
+    ],
+  },
+];
+
+export interface IData {
+  grades: { gradeId: string; value: string }[];
+  subjects: { subjectId: string; value: string }[];
+}
+
+// {
+//   grades: [{ gradeId: string; value: string; }];
+//   subjects: [{ subjectId: string; value: string; }];
+// }
+export const getSectionData = async (): Promise<IData[]> => {
+  // const { data } = await axiosWithAuth().get('/api/rumble/data');
+  // return data;
+
+  return Promise.resolve(dummyData);
+};
