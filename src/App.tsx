8ee3ff09f45bde9ef1e901ee10022d6762e1eb4f
B-Griffinn <bigriffin94@outlook.com>
2021-02-23 20:04:48 -0800

    Merge conflicts are resolved

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,9 @@ import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { TermsOfService } from './components/common/TermsOfService';
 import { AuthPage } from './components/pages/AuthPage';
-<<<<<<< HEAD
 import { TeacherDash } from './components/pages/DashBoards/TeacherDashboard';
-=======
 import { StudentDashboard } from './components/pages/StudentDashboard';
 
->>>>>>> f44dcfd6e681a6dca4d646c4344240c8c749324d
 const App = (): React.ReactElement => {
   return (
     <div className="App">
@@ -16,11 +13,8 @@ const App = (): React.ReactElement => {
         <Route path="/login" component={AuthPage} />
         <Route path="/tos" component={TermsOfService} />
         {/* Private Routes */}
-<<<<<<< HEAD
         <Route path="/dashboard/teacher" component={TeacherDash} />
-=======
         <Route path="/dashboard/student" component={StudentDashboard} />
->>>>>>> f44dcfd6e681a6dca4d646c4344240c8c749324d
         {/* Fallback redirect to main app homepage! Change the 'to' property after setting up routes. */}
         <Route path="/" component={() => <Redirect to="/homepage" />} />
       </Switch>
