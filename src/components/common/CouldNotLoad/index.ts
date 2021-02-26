160268318616d7c6a3ffba1de6c9bf3f3acc9afc
B-Griffinn <bigriffin94@outlook.com>
2021-02-26 11:50:04 -0800

    Moved CouldNotLoad to common

---
+++ src/components/common/CouldNotLoad/index.ts
@@ -0,0 +1,1 @@
+export { default as CouldNotLoad } from './CouldNotLoad';
--- src/components/pages/TeacherDashboard/TeacherSectionList/TeacherSectionListContainer.tsx
+++ src/components/pages/TeacherDashboard/TeacherSectionList/TeacherSectionListContainer.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { Sections } from '../../../../api';
 import { auth, sections } from '../../../../state';
-import CouldNotLoad from '../../CouldNotLoad';
+import CouldNotLoad from '../../../common/CouldNotLoad/CouldNotLoad';
 import RenderTeacherSectionList from './RenderTeacherSectionList';
 
 const TeacherSectionListContainer = (): React.ReactElement => {
