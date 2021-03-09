627ddd9f1b08c261c074b0c74ad9dc7878caa397
B-Griffinn <bigriffin94@outlook.com>
2021-03-09 11:24:32 -0800

    Pulling new changes from DEV

--- src/components/common/PromptBox/PromptBoxContainer.tsx
+++ src/components/common/PromptBox/PromptBoxContainer.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { Prompts } from '../../../api';
+import { prompts } from '../../../state';
 import RenderPromptBox from './RenderPromptBox';
 
 /**
@@ -7,6 +10,22 @@ import RenderPromptBox from './RenderPromptBox';
 
 const PromptBoxContainer = (): React.ReactElement => {
   // Grab prompts from recoil state
+  const [prompt, setPrompt] = useRecoilState(prompts.currentPrompt);
+
+  // TODO - what will we use to replace `active` fomr "getTimeUntilEvent"? I assume nothing bc we want to leave the Submit button open for ever
+
+  useEffect(() => {
+    if (!prompt) {
+      Prompts.getCurrent()
+        .then(({ data }) => {
+          console.log('Current Prompt: ', data);
+          setPrompt(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, []);
 
   return (
     <div>
