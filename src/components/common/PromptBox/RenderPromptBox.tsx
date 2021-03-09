9ad72693529797d19e1325cb463ac824e2f161dc
B-Griffinn <bigriffin94@outlook.com>
2021-03-09 12:25:29 -0800

    Rendering Prompt box update: IPrompt interface now has a submitted boolean, RenderPromptBox needs more discussion on modal handling, authModalOpen needs more discussion

--- src/components/common/PromptBox/RenderPromptBox.tsx
+++ src/components/common/PromptBox/RenderPromptBox.tsx
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { auth, prompts } from '../../../state';
+import { Modal } from '../Modal';
+import SubmissionForm from './SubmissionForm';
 
 const RenderPromptBox = (): React.ReactElement => {
+  // Store the prompt in a var from the RecoilStateValue
+  const prompt = useRecoilValue(prompts.currentPrompt);
+  // Store the user logged in status in a variable from RecoilStateValue
+  const isLogged = useRecoilValue(auth.isLoggedIn);
+
+  // TODO - I do not think we will need this, will we?
+  const setAuthModalVisible = useSetRecoilState(auth.authModalOpen);
+
+  // Modal State Handlers
+  const [showModal, setShowModal] = useState(false);
+  const toggleModal = () => {
+    if (isLogged) setShowModal((cur) => !cur);
+    else setAuthModalVisible(true);
+  };
+
   return (
     <div className="prompt-box">
-      <p>Render a modal component here</p>
-      <p>If prompt then show the prompt otherwise show a loading sign</p>
+      {isLogged && (
+        <Modal.Component
+          className="submissions"
+          component={(props) => <SubmissionForm />}
+          visible={showModal}
+          setVisible={setShowModal}
+          centered={!!isLogged}
+          title={isLogged ? 'Submit your Story' : ''}
+        />
+      )}
+      {prompt ? (
+        <>
+          <h2>Today&apos;s Prompt</h2>
+          <p>{prompt.prompt}</p>
+        </>
+      ) : (
+        <>
+          <p>WHERE IS THE PROMPT?!</p>
+        </>
+      )}
+      <div className="prompt-footer">
+        <button onClick={toggleModal} disabled={prompt?.submitted}>
+          {prompt?.submitted ? 'Submission Received!' : 'Submit Your Story'}
+        </button>
+      </div>
     </div>
   );
 };
