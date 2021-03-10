5212c2b5527a2b224d65d914e0ce683fc5c9167b
victor dronov <victordronov1@gmail.com>
2021-03-10 13:01:12 -0500

    basic file stuff added

--- src/components/pages/StudentDashboard/RenderStudentDashboard.tsx
+++ src/components/pages/StudentDashboard/RenderStudentDashboard.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Rumbles, Sections } from '../../../api';
 import { StudentRumbleList } from './StudentRumbleList';
-
+import { StudentViewRumble } from './StudentViewRumble';
 // Dislay Component for Students to view their sections
 const RenderStudentDashboard = ({
   rumbleList,
-  sectionList,
-}: IRenderStudentDashboardProps): React.ReactElement => {
+}: // sectionList,
+IRenderStudentDashboardProps): React.ReactElement => {
   return (
     <div className="student-dashboard">
       <h1>Your Dashboard</h1>
       <StudentRumbleList rumbleList={rumbleList} />
+      {/* \/ \/ \/ ***THIS IS TEMP *** \/ \/ \/ */}
+      <StudentViewRumble />
+      {/* ^^^^^^^^^^^^ */}
     </div>
   );
 };
---
+++ src/components/pages/StudentDashboard/StudentViewRumble/StudentViewRumbleContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const teacher = 'PENGUIN';
+
+const StudentViewRumble = (): React.ReactElement => {
+  return (
+    <div>
+      <h1>Get ready to write! Mr.{teacher} will start the rumble shortly.</h1>
+    </div>
+  );
+};
+
+export default StudentViewRumble;
