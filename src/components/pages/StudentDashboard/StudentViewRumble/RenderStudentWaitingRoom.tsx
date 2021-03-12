3220bc39aa151d43df3dd4d81fd134f7e046f73f
victor dronov <victordronov1@gmail.com>
2021-03-12 13:46:20 -0500

    waiting room call made

---
+++ src/components/pages/StudentDashboard/StudentViewRumble/RenderStudentWaitingRoom.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RenderStudentWaitingRoom = (): React.ReactElement => {
+  return <div>Please Wait For Your Teacher To Start The Rumble</div>;
+};
+
+export default RenderStudentWaitingRoom;
