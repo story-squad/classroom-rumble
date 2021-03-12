2ada2fd807f34fcf1920adfc102387a182af60b1
Brandon Ramirez <bran.ramirez.don@gmail.com>
2021-03-12 13:03:49 -0800

    Added checkbox group component, reworked new rumble to support it, styled create rumble form.

--- src/components/pages/TeacherDashboard/CreateNewRumble/CreateNewRumbleForm.tsx
+++ src/components/pages/TeacherDashboard/CreateNewRumble/CreateNewRumbleForm.tsx
@@ -1,45 +1,25 @@
 import React, { useMemo } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useHistory } from 'react-router-dom';
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { Prompts, Rumbles } from '../../../../api';
 import { IRumblePostBody } from '../../../../api/Rumbles';
-import { auth, prompts, rumbles, sections } from '../../../../state';
+import { auth, rumbles, sections } from '../../../../state';
 import { CheckboxGroup, Input, Select } from '../../../common';
 
 const CreateNewRumbleForm = ({
-  defaultSelected,
+  prompt,
 }: ICreateNewRumbleFormProps): React.ReactElement => {
+  // Functional Hooks
   const { register, handleSubmit } = useForm();
   const { push } = useHistory();
 
-  const [promptList, setPromptList] = useRecoilState(prompts.list);
-  const [promptOffset, setPromptOffset] = useRecoilState(prompts.promptOffset);
-
-  const promptQueue = useRecoilValue(prompts.queue);
-  const customPrompts = useRecoilValue(prompts.customList);
+  // Subscribe to state
   const sectionList = useRecoilValue(sections.list);
   const user = useRecoilValue(auth.user);
-
   const addRumbles = useSetRecoilState(rumbles.addRumbles);
 
-  const promptOptions = useMemo<Select.IOption[]>(() => {
-    const op: Select.IOption[] = [];
-    promptList.forEach((p) => op.push({ value: p.id, label: p.prompt }));
-    // Add custom unique prompts to the option list!
-    customPrompts.forEach((p) => {
-      if (!op.some((opItem) => opItem.value === p.id)) {
-        op.push({ value: p.id, label: p.prompt });
-      }
-    });
-    // Restrict duplicates from showing in the select!
-    promptQueue?.forEach((p) => {
-      if (!op.some((opItem) => opItem.value === p.id)) {
-        op.push({ value: p.id, label: p.prompt });
-      }
-    });
-    return op;
-  }, [promptList, promptQueue, customPrompts]);
+  // Parse the user's section list into a usable option type
   const sectionOptions = useMemo<Select.IOption<number>[]>(
     () => sectionList?.map((s) => ({ value: s.id, label: s.name })) ?? [],
     [sectionList],
@@ -48,17 +28,17 @@ const CreateNewRumbleForm = ({
   const onSubmit: SubmitHandler<
     IRumblePostBody & { sectionIds: string[] }
   > = async ({ sectionIds, ...data }) => {
-    console.log(sectionIds);
+    // Parse the ids that have been checked (sectionId[n] is TRUE)
+    // return the `value` of the option item at that index
     const idList = sectionOptions
       .filter((op, i) => sectionIds[i])
       .map((op) => op.value);
-    console.log(idList);
     try {
       if (user) {
         const res = await Rumbles.create(
           {
             numMinutes: parseInt(`${data.numMinutes}`, 10),
-            promptId: parseInt(`${data.promptId}`, 10),
+            promptId: prompt.id,
           },
           user.id,
           idList,
@@ -71,54 +51,43 @@ const CreateNewRumbleForm = ({
     }
   };
 
-  const loadMorePrompts = async () => {
-    try {
-      const promptResponse = await Prompts.getPrompts(promptOffset);
-      setPromptList((prev) => [...prev, ...promptResponse]);
-      setPromptOffset((prev) => prev + 5);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  const goBack = () => push('/dashboard/teacher');
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <Select.Component
-        id="promptIdNewRumble"
-        name="promptId"
-        register={register}
-        options={promptOptions}
-        defaultValue={defaultSelected?.id}
-      />
-      <button onClick={loadMorePrompts} type="button">
-        Load More Prompts
-      </button>
-
-      <Input
-        id="newRumbleNumMinutes"
-        name="numMinutes"
-        label="Submission Time (minutes)"
-        register={register}
-        type="number"
-        rules={{
-          min: 5 || 'Must be at least 5 minutes!',
-        }}
-      />
-
-      <CheckboxGroup
-        id="newRumbleSectionIds"
-        name="sectionIds"
-        register={register}
-        options={sectionOptions}
-      />
-
-      <input type="submit" value="Submit" />
+      <div className="section-wrapper">
+        <h3>Select Class(es)</h3>
+        <CheckboxGroup
+          id="newRumbleSectionIds"
+          name="sectionIds"
+          register={register}
+          options={sectionOptions}
+        />
+      </div>
+      <div className="section-wrapper">
+        <h3>Time to Submit (minutes)</h3>
+        <Input
+          id="newRumbleNumMinutes"
+          name="numMinutes"
+          register={register}
+          type="number"
+          rules={{
+            min: 5 || 'Must be at least 5 minutes!',
+          }}
+        />
+      </div>
+      <div className="button-row">
+        <button type="button" onClick={goBack}>
+          Cancel
+        </button>
+        <input type="submit" value="Create" />
+      </div>
     </form>
   );
 };
 
 interface ICreateNewRumbleFormProps {
-  defaultSelected?: Prompts.IPrompt;
+  prompt: Prompts.IPrompt;
 }
 
 export default CreateNewRumbleForm;
