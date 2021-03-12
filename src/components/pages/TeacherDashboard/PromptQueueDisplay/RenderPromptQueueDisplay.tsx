aff1d346a0cafd7d77fc35c575f533c9cb71ef53
Brandon Ramirez <bran.ramirez.don@gmail.com>
2021-03-11 19:21:09 -0800

    Added styling for prompt queue view on teacher dash:

--- src/components/pages/TeacherDashboard/PromptQueueDisplay/RenderPromptQueueDisplay.tsx
+++ src/components/pages/TeacherDashboard/PromptQueueDisplay/RenderPromptQueueDisplay.tsx
@@ -7,24 +7,25 @@ const RenderPromptQueueDisplay = ({
   queue,
 }: IRenderPromptQueueDisplayProps): React.ReactElement => {
   const [newPromptModalOpen, setNewPromptModalOpen] = useState(false);
+  const openModal = () => setNewPromptModalOpen(true);
   return (
-    <>
-      <CreateCustomPrompt
-        isVisible={newPromptModalOpen}
-        setIsVisible={setNewPromptModalOpen}
-      />
-      <div className="prompt-queue-display">
-        <h2>Prompt Queue</h2>
-        <button onClick={() => setNewPromptModalOpen(true)}>
-          Custom Prompt
-        </button>
+    <div className="prompt-queue-wrapper">
+      <div className="prompt-queue-container">
+        <CreateCustomPrompt
+          isVisible={newPromptModalOpen}
+          setIsVisible={setNewPromptModalOpen}
+        />
+        <h2>Select a Prompt to Start a Rumble</h2>
         <div className="prompt-queue-list">
           {queue.map((prompt) => (
             <PromptQueueItem key={prompt.id} {...prompt} />
           ))}
+          <div className="custom-prompt-button" onClick={openModal}>
+            <h3>Create Custom Prompt</h3>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
