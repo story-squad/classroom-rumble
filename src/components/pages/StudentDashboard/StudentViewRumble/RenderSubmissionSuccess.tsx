0c42f86976b7047c0804bdb11500059de58717f6
B-Griffinn <bigriffin94@outlook.com>
2021-03-16 12:13:42 -0700

    Render Submission Success Page complete

--- src/components/pages/StudentDashboard/StudentViewRumble/RenderSubmissionSuccess.tsx
+++ src/components/pages/StudentDashboard/StudentViewRumble/RenderSubmissionSuccess.tsx
@@ -3,7 +3,6 @@ import surfer from '../../../../utils/images/female-surfer.svg';
 const RenderSubmissionSuccess = (): React.ReactElement => {
   return (
     <div className="submission-success-wrapper">
-      <h2>DATE HERE</h2>
       <div className="success-image-wrapper">
         <img src={surfer} alt="submission-successful" />
       </div>
