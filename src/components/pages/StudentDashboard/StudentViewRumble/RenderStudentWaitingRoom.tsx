8ab8decaabbeb2536183d64070390fe6775412e7
B-Griffinn <bigriffin94@outlook.com>
2021-03-16 15:14:40 -0700

    Past Rumble Details skeleton created; Headers added to student dashboard components.

--- src/components/pages/StudentDashboard/StudentViewRumble/RenderStudentWaitingRoom.tsx
+++ src/components/pages/StudentDashboard/StudentViewRumble/RenderStudentWaitingRoom.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Header } from '../../../common';
 
 const RenderStudentWaitingRoom = (): React.ReactElement => {
   return (
-    <div className="waiting-room-wrapper">
-      Please Wait For Your Teacher To Start The Rumble
-    </div>
+    <>
+      <Header />
+      <div className="waiting-room-wrapper">
+        Please Wait For Your Teacher To Start The Rumble
+      </div>
+    </>
   );
 };
 
