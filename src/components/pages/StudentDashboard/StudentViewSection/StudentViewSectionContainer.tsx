50e2f19d4fc7c852a25bde03c3d3148d36f486d6
Steven Sussman <sjsussman@gmail.com>
2021-03-17 12:21:34 -0400

    created container and render page

---
+++ src/components/pages/StudentDashboard/StudentViewSection/StudentViewSectionContainer.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { useCheckBrowserState } from '../../../../hooks';
+import { current } from '../../../../state';
+import RenderTeacherViewSection from './RenderTeacherViewSection';
+
+const TeacherViewSectionContainer = (): React.ReactElement => {
+  const { isLoading } = useCheckBrowserState('section');
+  const section = useRecoilValue(current.section);
+
+  return section && !isLoading ? (
+    <RenderTeacherViewSection section={section} />
+  ) : isLoading ? (
+    <p>Loading...</p>
+  ) : (
+    <p>Redirecting...</p>
+  );
+};
+
+export default TeacherViewSectionContainer;
