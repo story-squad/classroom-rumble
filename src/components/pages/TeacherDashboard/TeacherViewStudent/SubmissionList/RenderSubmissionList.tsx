99b4f932738ffeb3f0616245369006e7834059d7
Brandon Ramirez <bran.ramirez.don@gmail.com>
2021-03-16 23:59:48 -0700

    Finished styling all of teacher dashboard. Needs more features though.

--- src/components/pages/TeacherDashboard/TeacherViewStudent/SubmissionList/RenderSubmissionList.tsx
+++ src/components/pages/TeacherDashboard/TeacherViewStudent/SubmissionList/RenderSubmissionList.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Auth, Submissions } from '../../../../../api';
+import { Table } from '../../../../common';
 import SubmissionCard from './SubmissionCard';
 
 const RenderSubmissionList = ({
   submissionList,
-  student,
 }: IRenderSubmissionListProps): React.ReactElement => {
   return (
     <div className="submission-list-wrapper">
-      <h2>Submissions</h2>
-      <div className="submission-list">
-        {submissionList.map((sub) => (
-          <SubmissionCard {...sub} key={sub.id} />
-        ))}
+      <div className="submission-list-container">
+        <div className="submission-list">
+          <Table.Header>
+            <Table.Col>Prompt</Table.Col>
+            <Table.Col>Score</Table.Col>
+          </Table.Header>
+          <Table.Body>
+            {submissionList.map((sub) => (
+              <SubmissionCard {...sub} key={sub.id} />
+            ))}
+          </Table.Body>
+        </div>
       </div>
     </div>
   );
