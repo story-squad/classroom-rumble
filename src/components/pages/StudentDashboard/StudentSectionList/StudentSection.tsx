55423cb7b74b0aab44d0b15893ce8db8bed4e456
Steven Sussman <sjsussman@gmail.com>
2021-03-18 10:36:43 -0400

    section info now loads

--- src/components/pages/StudentDashboard/StudentSectionList/StudentSection.tsx
+++ src/components/pages/StudentDashboard/StudentSectionList/StudentSection.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { Auth } from '../../../../api';
 import { ISection } from '../../../../api/Sections';
+import { auth } from '../../../../state';
 
 const StudentSection = ({
   active,
@@ -9,8 +13,18 @@ const StudentSection = ({
   name,
   subjectId,
 }: ISection): React.ReactElement => {
+  const { push } = useHistory();
+  const user = useRecoilValue(auth.user);
+  const role = useMemo(
+    () => (user?.roleId === Auth.Roles.user ? 'student' : 'teacher'),
+    [user],
+  );
+
+  const openSectionList = () => {
+    push(`/dashboard/${role}/section`);
+  };
   return (
-    <div className="student-section">
+    <div className="student-section" onClick={openSectionList}>
       <p>ID: {id}</p>
       <p>Name: {name} </p>
       <p>Grade ID: {gradeId}</p>
