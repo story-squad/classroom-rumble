f7edd284f2424dd7d7ef023c58e6a7d709626a73
B-Griffinn <bigriffin94@outlook.com>
2021-03-18 18:12:27 -0700

    Correctly getting the userID & rumbleId: for fetchig a submission

--- src/components/pages/StudentDashboard/StudentViewPastRumbleDetails/StudentViewPastRumbleDetailsContainer.tsx
+++ src/components/pages/StudentDashboard/StudentViewPastRumbleDetails/StudentViewPastRumbleDetailsContainer.tsx
@@ -2,17 +2,21 @@ import React, { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { Students } from '../../../../api';
 import { ISubItem } from '../../../../api/Submissions';
-import { current } from '../../../../state';
+import { auth, current } from '../../../../state';
 import { CouldNotLoad, Loader } from '../../../common';
 import RenderStudentViewPastRumbleDetails from './RenderStudentViewPastRumbleDetails';
 const StudentViewPastRumbleDetailsContainer = (): React.ReactElement => {
   const rumble = useRecoilValue(current.rumble);
-  const student = useRecoilValue(current.student);
+  const user = useRecoilValue(auth.user);
   const [submission, setSubmission] = useState<ISubItem[]>();
   const [error, setError] = useState<null | string>(null);
 
+  const rumbleId = rumble?.id;
+  const userId = user?.id;
+
   useEffect(() => {
-    Students.getSubForRumble(rumble?.id as number, student?.id as number)
+    console.log('peepee', { rumbleId, userId });
+    Students.getSubForRumble(rumbleId as number, userId as number)
       .then((res) => {
         console.log(res);
         setSubmission(res);
