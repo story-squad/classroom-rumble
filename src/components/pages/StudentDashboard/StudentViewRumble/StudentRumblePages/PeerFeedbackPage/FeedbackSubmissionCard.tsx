e6b83b33a214d609ce27d434f20df68b21045eeb
April Darger <april.m.darger@gmail.com>
2021-04-05 16:31:46 -0600

    Creates feedback submission cards.

---
+++ src/components/pages/StudentDashboard/StudentViewRumble/StudentRumblePages/PeerFeedbackPage/FeedbackSubmissionCard.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+const FeedbackSubmissionCard = (): React.ReactElement => {
+  return (
+    <div>
+      <div>
+        <img src={submission.src}></img>
+        <a>View Larger Image</a>
+        {/* modal to larger image */}
+      </div>
+      <form></form>
+    </div>
+  );
+};
+s; // {
+//   codename: 'A Codename',
+//   id: 1,
+//   prompt: 'This is a third test propmt.',
+//   rotation: 0,
+//   score: 0,
+//   src:
+//     'https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/02/16/08/google-chloe-letter-edit.jpg?width=1368&auto=webp&quality=75',
+//   userId: 1,
+// },
