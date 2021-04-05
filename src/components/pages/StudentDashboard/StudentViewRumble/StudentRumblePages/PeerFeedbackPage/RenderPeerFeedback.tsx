62189ff8e01c6d4ad2c1d7082013d11dca119fda
April Darger <april.m.darger@gmail.com>
2021-04-05 16:31:05 -0600

    Call to faker API and pass into FeedbackSubmissionCard(s).

--- src/components/pages/StudentDashboard/StudentViewRumble/StudentRumblePages/PeerFeedbackPage/RenderPeerFeedback.tsx
+++ src/components/pages/StudentDashboard/StudentViewRumble/StudentRumblePages/PeerFeedbackPage/RenderPeerFeedback.tsx
@@ -1,22 +1,50 @@
-import React from 'react';
-import { Sections } from '../../../../../../api';
+import React, { useEffect, useState } from 'react';
+import { FormProvider, useForm, useFormContext } from 'react-hook-form';
+import { Sections, Submissions } from '../../../../../../api';
+import { ISubItem } from '../../../../../../api/Submissions';
 import { PromptBox, SectionInfo } from '../../../../../common';
+import { FeedbackSubmissionCard } from './FeedbackSubmissionCard';
+
 // TODO - we will eventually need the 3 submissions that are assigned by the DS team in order to render those 3 submissions in a modal for students to read and provide feedback on.
+
+export const ConnectForm = ({ children }) => {
+  const methods = useFormContext();
+  return children({ ...methods });
+};
 
 const RenderPeerFeedback = ({
   section,
 }: IRenderPeerFeedbackProps): React.ReactElement => {
+  const methods = useForm();
+  const [submissions, setSubmissions] = useState<ISubItem[]>([]);
+  useEffect(() => {
+    Submissions.getSubmissionsForFeedback()
+      .then((res) => {
+        setSubmissions(res);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div className="feedback-wrapper">
       <SectionInfo section={section} />
       <PromptBox />
+      <FormProvider {...methods}>
+        <div className="submission-list">
+          {submissions.map((submission) => (
+            <FeedbackSubmissionCard
+              key={submission.id}
+              submission={submission}
+            />
+          ))}
+        </div>
+      </FormProvider>
     </div>
   );
 };
 
 interface IRenderPeerFeedbackProps {
   section: Sections.ISectionWithRumbles;
-  //   submission: Submissions.ISubItem[];
+  // submission: Submissions.ISubItem[];
 }
 
 export default RenderPeerFeedback;
