37f35a798357e612a6b4c153b028fcd26b599ac3
victor dronov <victordronov1@gmail.com>
2021-04-05 15:12:00 -0400

    waiting room styles

--- src/components/pages/StudentDashboard/StudentViewRumble/StudentRumblePages/WaitingRoom.tsx
+++ src/components/pages/StudentDashboard/StudentViewRumble/StudentRumblePages/WaitingRoom.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import logo from '../../../../../assets/img/waiting-time.svg';
 
 const WaitingRoom = (): React.ReactElement => {
   return (
     <div className="waiting-room-wrapper">
+      <img src={logo} />
       Please Wait For Your Teacher To Start The Rumble
     </div>
   );
