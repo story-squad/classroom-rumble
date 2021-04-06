3dbdb5055095dbd406083b7a9bef1ca3eee3e053
April Darger <april.m.darger@gmail.com>
2021-04-06 12:14:53 -0600

    Accepts props from parent and begins creating feedback form.

--- src/components/pages/StudentDashboard/StudentViewRumble/StudentRumblePages/PeerFeedbackPage/FeedbackSubmissionCard.tsx
+++ src/components/pages/StudentDashboard/StudentViewRumble/StudentRumblePages/PeerFeedbackPage/FeedbackSubmissionCard.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-const FeedbackSubmissionCard = (): React.ReactElement => {
+import { Submissions } from '../../../../../../api';
+
+const FeedbackSubmissionCard = ({
+  submission,
+}: IFeedbackSubmissionCardProps): React.ReactElement => {
   return (
     <div>
       <div>
@@ -7,17 +11,19 @@ const FeedbackSubmissionCard = (): React.ReactElement => {
         <a>View Larger Image</a>
         {/* modal to larger image */}
       </div>
-      <form></form>
+      <form>
+        <h2>FEEDBACK</h2>
+        <label>
+          How much did you want the main characters to succeed?
+          <input type="radio"></input>
+        </label>
+      </form>
     </div>
   );
 };
-s; // {
-//   codename: 'A Codename',
-//   id: 1,
-//   prompt: 'This is a third test propmt.',
-//   rotation: 0,
-//   score: 0,
-//   src:
-//     'https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/02/16/08/google-chloe-letter-edit.jpg?width=1368&auto=webp&quality=75',
-//   userId: 1,
-// },
+
+interface IFeedbackSubmissionCardProps {
+  submission: Submissions.ISubItem;
+}
+
+export default FeedbackSubmissionCard;
