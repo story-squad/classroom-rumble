86c5428d3dd86e935c6e65c1fd32a3e56a675bf9
April Darger <april.m.darger@gmail.com>
2021-04-06 12:13:33 -0600

    Cleans up page by moving sections of code to new feedback submission list and card.

--- src/components/pages/StudentDashboard/StudentViewRumble/StudentRumblePages/PeerFeedbackPage/RenderPeerFeedback.tsx
+++ src/components/pages/StudentDashboard/StudentViewRumble/StudentRumblePages/PeerFeedbackPage/RenderPeerFeedback.tsx
@@ -1,42 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { FormProvider, useForm, useFormContext } from 'react-hook-form';
-import { Sections, Submissions } from '../../../../../../api';
-import { ISubItem } from '../../../../../../api/Submissions';
+import React from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import { Sections } from '../../../../../../api';
 import { PromptBox, SectionInfo } from '../../../../../common';
-import { FeedbackSubmissionCard } from './FeedbackSubmissionCard';
+import FeedbackSubmissionList from './FeedbackSubmissionList';
 
 // TODO - we will eventually need the 3 submissions that are assigned by the DS team in order to render those 3 submissions in a modal for students to read and provide feedback on.
-
-export const ConnectForm = ({ children }) => {
-  const methods = useFormContext();
-  return children({ ...methods });
-};
 
 const RenderPeerFeedback = ({
   section,
 }: IRenderPeerFeedbackProps): React.ReactElement => {
   const methods = useForm();
-  const [submissions, setSubmissions] = useState<ISubItem[]>([]);
-  useEffect(() => {
-    Submissions.getSubmissionsForFeedback()
-      .then((res) => {
-        setSubmissions(res);
-      })
-      .catch((err) => console.log(err));
-  }, []);
+
   return (
     <div className="feedback-wrapper">
       <SectionInfo section={section} />
       <PromptBox />
       <FormProvider {...methods}>
-        <div className="submission-list">
-          {submissions.map((submission) => (
-            <FeedbackSubmissionCard
-              key={submission.id}
-              submission={submission}
-            />
-          ))}
-        </div>
+        <FeedbackSubmissionList />
       </FormProvider>
     </div>
   );
