dff40904f8ce42d106013e9779431eacfb583f68
April Darger <april.m.darger@gmail.com>
2021-04-09 11:00:26 -0600

    Adds loading and error logic, starts the logic to disable the submit button.

--- src/components/pages/StudentDashboard/StudentViewRumble/StudentRumblePages/PeerFeedbackPage/FeedbackSubmissionList.tsx
+++ src/components/pages/StudentDashboard/StudentViewRumble/StudentRumblePages/PeerFeedbackPage/FeedbackSubmissionList.tsx
@@ -1,24 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import { SubmitHandler, useFormContext } from 'react-hook-form';
+import { SubmitHandler, useForm, useFormContext } from 'react-hook-form';
 import { Submissions } from '../../../../../../api';
 import { ISubItem } from '../../../../../../api/Submissions';
+import { CouldNotLoad } from '../../../../../common/CouldNotLoad';
+import { Loader } from '../../../../../common/Loader';
 import FeedbackSubmissionCard from './FeedbackSubmissionCard';
 
 const FeedbackSubmissionList = (): React.ReactElement => {
-  const [submissions, setSubmissions] = useState<ISubItem[]>([]);
+  const [submissions, setSubmissions] = useState<ISubItem[]>();
+  const [error, setError] = useState<null | string>(null);
+  const { handleSubmit } = useFormContext();
+  const { formState } = useForm();
+
   useEffect(() => {
     Submissions.getSubmissionsForFeedback()
       .then((res) => {
         setSubmissions(res);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log({ err });
+        setError('There are no user submissions for feedback.');
+      });
   }, []);
 
-  const { handleSubmit } = useFormContext();
-  const onSubmit: SubmitHandler<Record<string, unknown>> = (data) =>
+  const onSubmit: SubmitHandler<Record<string, unknown>> = (data) => {
     console.log(data);
+  };
 
-  return (
+  return submissions ? (
     <div className="submission-list">
       {submissions.map((submission, index) => (
         <FeedbackSubmissionCard
@@ -27,9 +36,14 @@ const FeedbackSubmissionList = (): React.ReactElement => {
           subNumber={index + 1}
         />
       ))}
-      <button onClick={handleSubmit(onSubmit)}>Submit</button>
-      {/* Validation? */}
+      <button onClick={handleSubmit(onSubmit)} disabled={!formState.isValid}>
+        Submit
+      </button>
     </div>
+  ) : error ? (
+    <CouldNotLoad error={error} />
+  ) : (
+    <Loader message="Loading feedback forms" />
   );
 };
 
