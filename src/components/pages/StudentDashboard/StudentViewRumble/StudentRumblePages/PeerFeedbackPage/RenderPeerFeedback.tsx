8a42889c4c617a4acf1f1074e2ca2a38539a3bf0
April Darger <april.m.darger@gmail.com>
2021-04-12 11:21:59 -0600

    Disables Submit button. Moves Form and logic from unnecessary component.

--- src/components/pages/StudentDashboard/StudentViewRumble/StudentRumblePages/PeerFeedbackPage/RenderPeerFeedback.tsx
+++ src/components/pages/StudentDashboard/StudentViewRumble/StudentRumblePages/PeerFeedbackPage/RenderPeerFeedback.tsx
@@ -1,27 +1,59 @@
 import React from 'react';
-import { FormProvider, useForm } from 'react-hook-form';
-import { Sections } from '../../../../../../api';
-import { PromptBox, SectionInfo } from '../../../../../common';
-import FeedbackSubmissionList from './FeedbackSubmissionList';
+import {
+  FormProvider,
+  SubmitHandler,
+  useForm,
+  useFormContext,
+} from 'react-hook-form';
+import { Sections, Submissions } from '../../../../../../api';
+import { CouldNotLoad, PromptBox, SectionInfo } from '../../../../../common';
+import { Loader } from '../../../../../common/Loader';
+import FeedbackSubmissionCard from './FeedbackSubmissionCard';
 
 const RenderPeerFeedback = ({
   section,
+  error,
+  submissions,
 }: IRenderPeerFeedbackProps): React.ReactElement => {
-  const methods = useForm();
+  const methods = useForm({
+    mode: 'onChange',
+    reValidateMode: 'onChange',
+  });
+  const { handleSubmit, formState } = useFormContext();
 
-  return (
+  const onSubmit: SubmitHandler<Record<string, unknown>> = (data) => {
+    console.log(data);
+  };
+
+  return submissions ? (
     <div className="feedback-wrapper">
       <SectionInfo section={section} />
       <PromptBox />
       <FormProvider {...methods}>
-        <FeedbackSubmissionList />
+        <form className="submission-list" onSubmit={handleSubmit(onSubmit)}>
+          {submissions.map((submission, index) => (
+            <FeedbackSubmissionCard
+              key={submission.id}
+              submission={submission}
+              subNumber={index + 1}
+            />
+          ))}
+          <button disabled={!formState.isValid}>Submit</button>
+        </form>
       </FormProvider>
     </div>
+  ) : error ? (
+    <CouldNotLoad error={error} />
+  ) : (
+    <Loader message="Loading feedback forms" />
   );
 };
 
 interface IRenderPeerFeedbackProps {
   section: Sections.ISectionWithRumbles;
+  // Ask about types for submission and error
+  submissions: Submissions.ISubItem[] | undefined;
+  error: string | null;
 }
 
 export default RenderPeerFeedback;
