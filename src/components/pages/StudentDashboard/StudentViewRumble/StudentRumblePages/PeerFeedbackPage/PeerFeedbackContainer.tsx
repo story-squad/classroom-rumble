21d219565596ca87912805279fe4dfda21f6d75b
April Darger <april.m.darger@gmail.com>
2021-04-12 11:23:13 -0600

    Moves API call (currently faker) from unneeded component.

--- src/components/pages/StudentDashboard/StudentViewRumble/StudentRumblePages/PeerFeedbackPage/PeerFeedbackContainer.tsx
+++ src/components/pages/StudentDashboard/StudentViewRumble/StudentRumblePages/PeerFeedbackPage/PeerFeedbackContainer.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
+import { Submissions } from '../../../../../../api';
 import { current } from '../../../../../../state';
 import { Loader } from '../../../../../common';
 import RenderPeerFeedback from './RenderPeerFeedback';
 
 const PeerFeedbackContainer = (): React.ReactElement => {
   const section = useRecoilValue(current.section);
+  const [error, setError] = useState<null | string>(null);
+  const [submissions, setSubmissions] = useState<Submissions.ISubItem[]>();
+
+  useEffect(() => {
+    Submissions.getSubmissionsForFeedback()
+      .then((res) => {
+        setSubmissions(res);
+      })
+      .catch((err) => {
+        console.log({ err });
+        setError('There are no user submissions for feedback.');
+      });
+  }, []);
 
   return section ? (
-    <RenderPeerFeedback section={section} />
+    <RenderPeerFeedback
+      section={section}
+      error={error}
+      submissions={submissions}
+    />
   ) : (
     <Loader message={'Loading Feedback'} />
   );
