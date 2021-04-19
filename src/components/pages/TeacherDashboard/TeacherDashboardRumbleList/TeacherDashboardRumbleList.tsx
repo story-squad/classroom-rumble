da96b37a426c3082bb679986f3a9e47455e8e766
victor dronov <victordronov1@gmail.com>
2021-04-19 17:22:41 -0400

    added img if there are no rumbles for teacher

--- src/components/pages/TeacherDashboard/TeacherDashboardRumbleList/TeacherDashboardRumbleList.tsx
+++ src/components/pages/TeacherDashboard/TeacherDashboardRumbleList/TeacherDashboardRumbleList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Sections } from '../../../../api';
+import emptyMail from '../../../../assets/img/empty_inbox.svg';
 import TeacherDashboardRumbleCard from './TeacherDashboardRumbleCard';
 
 const TeacherDashboardRumbleList = ({
@@ -12,14 +13,24 @@ const TeacherDashboardRumbleList = ({
       <div className="teacher-dash-rumble-list-container">
         <h2>Current Rumbles</h2>
         <div className="rumble-list">
-          {sections?.map((sec) =>
-            sec.rumbles.map((rum) => (
-              <TeacherDashboardRumbleCard
-                key={rum.id}
-                section={sec}
-                rumble={rum}
-              />
-            )),
+          {sections.length <= 0 ? (
+            <div>
+              <h3>Current Rumbles</h3>
+              <p>You don&apos;t have any current rumbles.</p>
+              <img src={emptyMail} alt="You don't have any current rumbles" />
+            </div>
+          ) : (
+            <>
+              {sections?.map((sec) =>
+                sec.rumbles.map((rum) => (
+                  <TeacherDashboardRumbleCard
+                    key={rum.id}
+                    section={sec}
+                    rumble={rum}
+                  />
+                )),
+              )}
+            </>
           )}
         </div>
       </div>
