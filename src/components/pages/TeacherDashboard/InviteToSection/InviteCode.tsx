30c285272b92faf3f58fe1eee5ee38396cbd4182
Steven Sussman <63059323+sjsussman@users.noreply.github.com>
2021-04-22 18:57:41 -0400

    Feature/add clipboard copy (#49)
    * added library
    
    * added copy functionality
    
    * updated button styling

--- src/components/pages/TeacherDashboard/InviteToSection/InviteCode.tsx
+++ src/components/pages/TeacherDashboard/InviteToSection/InviteCode.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useClipboard } from 'use-clipboard-copy';
 import { Sections } from '../../../../api';
 import { helpers } from '../../../../utils';
 
@@ -6,19 +7,29 @@ const InviteCode = ({
   section,
   goBack,
   disableSectionPicker,
-}: IInviteCodeProps): React.ReactElement => (
-  <div className="invite-code">
-    <div className="content">
-      <h3>{section.name}</h3>
-      <textarea rows={6} readOnly value={helpers.joinCode(section)} />
-    </div>
-    {!disableSectionPicker && (
-      <div className="footer">
-        <button onClick={goBack}>Pick a Different Class</button>
+}: IInviteCodeProps): React.ReactElement => {
+  const clipboard = useClipboard({ copiedTimeout: 750 });
+  return (
+    <div className="invite-code">
+      <div className="content">
+        <h3>{section.name}</h3>
+        <textarea
+          rows={6}
+          readOnly
+          value={clipboard.copied ? 'Copied!' : helpers.joinCode(section)}
+          ref={clipboard.target}
+          onClick={clipboard.copy}
+        />
       </div>
-    )}
-  </div>
-);
+      {!disableSectionPicker && (
+        <div className="footer">
+          <button onClick={clipboard.copy}>Click To Copy</button>
+          <button onClick={goBack}>Pick a Different Class</button>
+        </div>
+      )}
+    </div>
+  );
+};
 
 interface IInviteCodeProps {
   section: Sections.ISectionWithRumbles;
