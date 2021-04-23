3c2a8abdfc157a9041b423813f57e4ff0f95105f
victor-dronov <66633234+VictorDronov@users.noreply.github.com>
2021-04-23 14:03:37 -0400

    Refactor/teacher dashboard (#54)
    * added img if there are no rumbles for teacher
    
    * added logic that if teacher has no class an img shows if teacher has class and no rumbles class shows but rumbles area shows an img
    
    * some stuff works some stuf buggy and needs work
    
    * logic for no rumbles no sections and no students added
    
    * logic tweak when no students styles for when no students
    
    * styles and images for when there are no classes and rumbles added and styled
    
    * all styles done I think
    
    * style tweaks
    
    * done i think
    
    * style tweaks and non ts file name changes
    
    * import update and sections from props and not recoil state

--- src/components/pages/TeacherDashboard/TeacherDashboardSectionList/TeacherDashboardSectionList.tsx
+++ src/components/pages/TeacherDashboard/TeacherDashboardSectionList/TeacherDashboardSectionList.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { Sections } from '../../../../api';
+import rocketBoy from '../../../../assets/img/rocket_boy.svg';
+import talk from '../../../../assets/img/speech.svg';
 import { modals } from '../../../../state';
 import { InviteToSection } from '../InviteToSection';
 import { CreateNewSection } from './CreateNewSection';
@@ -13,28 +15,47 @@ const RenderTeacherDashboardSectionList = ({
   const [newSectionOpen, setNewSectionOpen] = useState(false);
   const openSectionModal = () => setNewSectionOpen(true);
   const openInviteModal = () => setInviteModalOpen(true);
+
   return (
-    <>
-      <CreateNewSection
-        isVisible={newSectionOpen}
-        setIsVisible={setNewSectionOpen}
-      />
-      <InviteToSection />
-      <div className="teacher-dash-section-list-wrapper">
-        <div className="teacher-dash-section-list-container">
-          <h2>Classes</h2>
-          <div className="button-row">
-            <button onClick={openSectionModal}>Add Class</button>
-            <button onClick={openInviteModal}>Invite to Class</button>
+    <div className="teacher-dash-section-list-wrapper">
+      <div className="teacher-dash-section-list-container">
+        <h2>Classes</h2>
+        <CreateNewSection
+          isVisible={newSectionOpen}
+          setIsVisible={setNewSectionOpen}
+        />
+        <InviteToSection />
+        {sections.length <= 0 ? (
+          <div className="no-sections">
+            <div>
+              <img
+                className="speach"
+                src={talk}
+                alt="You don't have any classes yet. Let's get started!"
+                onClick={openSectionModal}
+              />
+              <img
+                className="rocket"
+                src={rocketBoy}
+                alt="You don't have any classes"
+              />
+            </div>
           </div>
-          <div className="section-list">
-            {sections?.map((sec) => (
-              <Section {...sec} key={sec.id} />
-            ))}
-          </div>
-        </div>
+        ) : (
+          <>
+            <div className="button-row">
+              <button onClick={openSectionModal}>Add New Class</button>
+              <button onClick={openInviteModal}>Invite to Class</button>
+            </div>
+            <div className="section-list">
+              {sections?.map((sec) => (
+                <Section {...sec} key={sec.id} />
+              ))}
+            </div>
+          </>
+        )}
       </div>
-    </>
+    </div>
   );
 };
 
--- src/components/pages/TeacherDashboard/TeacherViewSection/SectionStudentList/RenderSectionStudentList.tsx
+++ src/components/pages/TeacherDashboard/TeacherViewSection/SectionStudentList/RenderSectionStudentList.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { useSetRecoilState } from 'recoil';
 import { Sections, Students } from '../../../../../api';
+import noStudents from '../../../../../assets/img/no_students.svg';
+import { modals } from '../../../../../state';
 import { Table } from '../../../../common';
 import SectionStudentCard from './SectionStudentCard';
 
@@ -7,25 +10,43 @@ const RenderSectionStudentList = ({
   studentList,
   section,
 }: IRenderSectionStudentListProps): React.ReactElement => {
+  const setInviteModalOpen = useSetRecoilState(modals.invite.isOpen);
+  const openInviteModal = () => setInviteModalOpen(true);
+  console.log(studentList);
+
   return (
     <div className="student-list-wrapper">
       <div className="student-list-container">
         <div className="student-list">
-          <Table.Header>
-            <Table.Col>Student Name</Table.Col>
-            <Table.Col>Last Name</Table.Col>
-            <Table.Col>First Name</Table.Col>
-            <Table.Col># of Submissions</Table.Col>
-          </Table.Header>
-          <Table.Body>
-            {studentList.map((student) => (
-              <SectionStudentCard
-                student={student}
-                section={section}
-                key={student.id}
-              />
-            ))}
-          </Table.Body>
+          {studentList.length > 0 ? (
+            <>
+              <Table.Header>
+                <Table.Col>Student Name</Table.Col>
+                <Table.Col>Last Name</Table.Col>
+                <Table.Col>First Name</Table.Col>
+                <Table.Col># of Submissions</Table.Col>
+              </Table.Header>
+              <Table.Body>
+                {studentList.map((student) => (
+                  <SectionStudentCard
+                    student={student}
+                    section={section}
+                    key={student.id}
+                  />
+                ))}
+              </Table.Body>
+            </>
+          ) : (
+            <div className="no-students">
+              <div className="message-text-container">
+                <p>There are no students in this class &nbsp;</p>
+                <span>
+                  <button onClick={openInviteModal}>Invite to Class</button>
+                </span>
+              </div>
+              <img src={noStudents} alt="you have no students" />
+            </div>
+          )}
         </div>
       </div>
     </div>
