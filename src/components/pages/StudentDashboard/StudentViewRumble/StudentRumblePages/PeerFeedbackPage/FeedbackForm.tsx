dfb6116666b0f285b75d2b79b4222418f336d37d
Liz Drumm Parry <lizdrumm@gmail.com>
2021-04-29 19:53:19 -0500

    Styling/peer feedback form (#61)
    * styling peer feedback form
    
    * styling peer feedback form
    
    * more styling
    
    * styling and deleted unused component
    
    * added border to mobile view
    
    * more styling of the form
    
    * added more breakpoints
    
    * a few more edits
    
    Co-authored-by: Brandon Ramirez <bran.ramirez.don@gmail.com>

--- src/components/pages/StudentDashboard/StudentViewRumble/StudentRumblePages/PeerFeedbackPage/FeedbackForm.tsx
+++ src/components/pages/StudentDashboard/StudentViewRumble/StudentRumblePages/PeerFeedbackPage/FeedbackForm.tsx
@@ -14,45 +14,65 @@ const FeedbackForm = ({
     { label: '4', value: '4' },
     { label: '5', value: '5' },
   ];
-
   return (
-    <div>
-      <h2>FEEDBACK</h2>
+    <div className="radio-button-section">
+      <h2 className="form-header">FEEDBACK</h2>
       <div>
-        <div>
-          <p>How much did you want the main characters to succeed?</p>
-          <RadioGroup
-            name={`Submission${subNumber}-Q1`}
-            register={register}
-            options={radioRange}
-            rules={{ required: 'Please choose a value from 1-5!' }}
-            errors={errors}
-          />
-        </div>
-        <div>
-          <p>
-            How interested were you in finding out what happens in the Story?
+        <div className="radio-wrapper">
+          <p className="form-questions">
+            How much did you want the main characters to succeed?
           </p>
-          <RadioGroup
-            name={`Submission${subNumber}-Q2`}
-            register={register}
-            options={radioRange}
-            rules={{ required: 'Please choose a value from 1-5!' }}
-            errors={errors}
-          />
+          <div className="radios">
+            <RadioGroup
+              name={`Submission${subNumber}-Q1`}
+              register={register}
+              options={radioRange}
+              rules={{ required: 'Please choose a value from 1-5!' }}
+              errors={errors}
+            />
+            <div className="footer-section">
+              <p>Not at all</p>
+              <p>A lot</p>
+            </div>
+          </div>
         </div>
-        <div>
-          <p>
+        <div className="radio-wrapper">
+          <p className="form-questions">
+            How interested were you in finding out what happens in the story?
+          </p>
+
+          <div className="radios">
+            <RadioGroup
+              name={`Submission${subNumber}-Q2`}
+              register={register}
+              options={radioRange}
+              rules={{ required: 'Please choose a value from 1-5!' }}
+              errors={errors}
+            />
+            <div className="footer-section">
+              <p>Not interested</p>
+              <p>Very interested</p>
+            </div>
+          </div>
+        </div>
+        <div className="radio-wrapper">
+          <p className="form-questions">
             How easily did the descriptions allow you to imagine the setting and
             action?
           </p>
-          <RadioGroup
-            name={`Submission${subNumber}-Q3`}
-            register={register}
-            options={radioRange}
-            rules={{ required: 'Please choose a value from 1-5!' }}
-            errors={errors}
-          />
+          <div className="radios">
+            <RadioGroup
+              name={`Submission${subNumber}-Q3`}
+              register={register}
+              options={radioRange}
+              rules={{ required: 'Please choose a value from 1-5!' }}
+              errors={errors}
+            />
+            <div className="footer-section">
+              <p>Not easily</p>
+              <p>Very easily</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
