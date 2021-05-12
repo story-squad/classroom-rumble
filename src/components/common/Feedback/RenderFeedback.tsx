21706f2bd89a02083f5ec96dcfe519a850ffb6fe
April Darger <april.m.darger@gmail.com>
2021-05-12 17:55:33 -0600

    Modifies table to match figma. Removes header, adds text to score.

--- src/components/common/Feedback/RenderFeedback.tsx
+++ src/components/common/Feedback/RenderFeedback.tsx
@@ -16,22 +16,22 @@ const RenderFeedback = ({
           // If there are no averages show that there is no feedback yet else show the table with feedback
           <div className="feedback-container">
             <h2>FEEDBACK</h2>
-            <Table.Header>
+            {/* <Table.Header>
               <Table.Col>Questions</Table.Col>
               <Table.Col>Rating out of 5</Table.Col>
-            </Table.Header>
+            </Table.Header> */}
             <Table.Body>
               <Table.Row>
                 <Table.Col>1. Is this the first question?</Table.Col>
-                <Table.Col>{averages.score1}</Table.Col>
+                <Table.Col>{averages.score1} out of 5</Table.Col>
               </Table.Row>
               <Table.Row>
                 <Table.Col>2. Is this the second question?</Table.Col>
-                <Table.Col>{averages.score2}</Table.Col>
+                <Table.Col>{averages.score2} out of 5</Table.Col>
               </Table.Row>
               <Table.Row>
                 <Table.Col>3. Is this the third question?</Table.Col>
-                <Table.Col>{averages.score3}</Table.Col>
+                <Table.Col>{averages.score3} out of 5</Table.Col>
               </Table.Row>
             </Table.Body>
           </div>
