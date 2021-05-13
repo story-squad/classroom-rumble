ae321f98e35e42237711e30c7df0f6d88592fd85
April Darger <april.m.darger@gmail.com>
2021-05-13 17:00:10 -0600

    Creates pop up for parent validation.

---
+++ src/components/pages/StudentDashboard/ParentValidationForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { useForm } from 'react-hook-form';
+
+const ParentValidationForm = (): React.ReactElement => {
+  // const { errors, register } = useForm({
+  //   mode: 'onChange',
+  // });
+
+  return <div>Hello Modal</div>;
+};
+
+export default ParentValidationForm;
