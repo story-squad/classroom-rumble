2c35d4fcad3bd7c2125addaf4ac5b5d25ef4e8ad
April Darger <april.m.darger@gmail.com>
2021-05-13 18:03:55 -0600

    Adds Parent Validation to menu drop down when child is not validated.

--- src/components/common/Header/Menu/Menu.tsx
+++ src/components/common/Header/Menu/Menu.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { auth, modals } from '../../../../state';
 import Logout from './Logout';
 
 // For future things to be added for the drop down
 
 const Menu = (): React.ReactElement => {
+  const userInfo = useRecoilValue(auth.user);
+  const setParentValidationOpen = useSetRecoilState(
+    modals.validationModalIsOpen,
+  );
+  const openParentValidationModal = () => setParentValidationOpen(true);
+
   return (
     <>
       <Logout />
+      {!userInfo?.isValidated && (
+        <div onClick={openParentValidationModal}>
+          <p>Parent Validation</p>
+        </div>
+      )}
     </>
   );
 };
