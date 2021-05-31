797437490811b434b871836af63ff90576b0f2e9
Brandon <bran.ramirez.don@gmail.com>
2021-05-31 12:34:17 -0700

    Added more relevant state resets.

--- src/state/reset.ts
+++ src/state/reset.ts
@@ -1,5 +1,17 @@
 import { selector } from 'recoil';
-import { authToken, user } from './authState';
+import { authToken, isLoggedIn, user } from './authState';
+import {
+  feedbackForSubmission,
+  hasSubmitted,
+  rumble,
+  section,
+  student,
+  sub,
+} from './currentViewState';
+import { currentPrompt, customList } from './promptState';
+import { addRumbles, list as rumbleList } from './rumbleState';
+import { list as sectionList } from './sectionState';
+import { list as studentList } from './studentState';
 
 export const reset = selector<undefined>({
   key: 'clearRecoilState',
@@ -7,5 +19,18 @@ export const reset = selector<undefined>({
   set: ({ reset }) => {
     reset(authToken);
     reset(user);
+    reset(isLoggedIn);
+    reset(hasSubmitted);
+    reset(feedbackForSubmission);
+    reset(rumble);
+    reset(section);
+    reset(student);
+    reset(sub);
+    reset(customList);
+    reset(currentPrompt);
+    reset(studentList);
+    reset(sectionList);
+    reset(rumbleList);
+    reset(addRumbles);
   },
 });
