f18997455d2824a4a9af1dcf1436ce093a767b9a
Liz Drumm <lizdrumm@gmail.com>
2021-06-03 11:50:30 -0500

    changed location of 'go back' button

--- src/components/common/CouldNotLoad/CouldNotLoad.tsx
+++ src/components/common/CouldNotLoad/CouldNotLoad.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ufo_cow from '../../../assets/img/alien.svg';
-import { Button } from '../Button';
 /**
  * Could Not Load will help us render consistent error messages throughout the application.
  * @param error The error the be loaded for the component, which in our case will be a message.
@@ -14,22 +13,10 @@ const CouldNotLoad = ({
 }: CouldNotLoadProps): React.ReactElement => {
   console.log({ error });
 
-  const goBack = async () => {
-    try {
-      await location.reload();
-    } catch (err) {
-      console.log({ err });
-    }
-  };
-
   return (
     <div className={`could-not-load${className ? ' ' + className : ''}`}>
       <img src={ufo_cow} />
-      <div className="message">{error}&#128557;</div>
-      <Button htmlType="button" onClick={goBack}>
-        Go back!
-      </Button>
-       
+      <div className="message">{error}&#128557;</div> 
     </div>
   );
 };
--- src/components/common/CouldNotLoad/CouldNotLoadModal.tsx
+++ src/components/common/CouldNotLoad/CouldNotLoadModal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Alert } from '../Alert';
+import { Button } from '../Button';
 import CouldNotLoad from './CouldNotLoad';
 
 const CouldNotLoadModal = ({
@@ -7,13 +8,22 @@ const CouldNotLoadModal = ({
   visible,
   setVisible,
 }: ICouldNotLoadModalProps): React.ReactElement => {
+  const closeAlert = () => {
+    setVisible(false);
+  };
+
   return (
     <Alert.Component
       className="could-not-load-modal"
       visible={visible}
       setVisible={setVisible}
       centered
-      component={(props) => <CouldNotLoad {...props} error={error} />}
+      component={(props) => (
+        <>
+          <CouldNotLoad {...props} error={error} />
+          <Button onClick={closeAlert}>GO BACK</Button>
+        </>
+      )}
     />
   );
 };
