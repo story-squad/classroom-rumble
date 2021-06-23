d6184b880636beccd4c2bdd6643452f7597b5168
victor dronov <victordronov1@gmail.com>
2021-06-23 18:07:52 -0400

    merge complete

--- src/components/common/FeedbackDisplay/FeedbackContainer.tsx
+++ src/components/common/FeedbackDisplay/FeedbackContainer.tsx
@@ -1,75 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import { useRecoilState } from 'recoil';
+import React, { useEffect } from 'react';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import { Feedback, Submissions } from '../../../api';
 import { useAsync } from '../../../hooks';
-import { current } from '../../../state';
+import { feedback, submissions } from '../../../state';
 import { Loader } from '../Loader';
-import { IAverages } from './feedbackTypes';
 import RenderFeedback from './RenderFeedback';
 
 const FeedbackContainer = ({
-  submission,
+  submissionId,
 }: IFeedbackContainerProps): React.ReactElement => {
-  const [feedback, setFeedback] = useRecoilState(current.feedbackForSubmission);
-  const [averages, setAverages] = useState<IAverages>();
+  const [submission, updateSubmission] = useRecoilState(
+    submissions.getById(submissionId),
+  );
+  const feedbackIds = useRecoilValue(feedback.getIdsBySubId(submissionId));
+  const averages = useRecoilValue(feedback.averages(feedbackIds));
+  const addFeedback = useSetRecoilState(feedback.add);
 
   const [getSubmissionFeedback, loading, ,] = useAsync({
     asyncFunction: Feedback.getSubmissionFeedback,
-    setter: setFeedback,
+    setter: addFeedback,
+  });
+
+  const [getSub, subLoading] = useAsync({
+    asyncFunction: Submissions.get,
+    setter: ([subFromAPI]) => updateSubmission(subFromAPI),
   });
 
   useEffect(() => {
-    if (submission) {
+    if (submission && !loading) {
       getSubmissionFeedback(submission.id);
+    } else if (!subLoading) {
+      getSub({ ids: [submissionId] });
     }
   }, [submission]);
 
-  useEffect(() => {
-    if (!feedback || feedback.length <= 0) return;
-
-    let score1 = 0;
-    let score2 = 0;
-    let score3 = 0;
-    const count = [0, 0, 0];
-
-    feedback.forEach((fb) => {
-      if (!fb) return;
-      else {
-        if (fb.score1 && fb.score1 > 0) {
-          score1 += fb.score1;
-          count[0]++;
-        }
-        if (fb.score2 && fb.score2 > 0) {
-          score2 += fb.score2;
-          count[1]++;
-        }
-        if (fb.score3 && fb.score3 > 0) {
-          score3 += fb.score3;
-          count[2]++;
-        }
-      }
-    });
-
-    score1 /= count[0];
-    score2 /= count[1];
-    score3 /= count[2];
-
-    setAverages({
-      score1: parseFloat(score1.toFixed(2)),
-      score2: parseFloat(score2.toFixed(2)),
-      score3: parseFloat(score3.toFixed(2)),
-    });
-  }, [feedback]);
-
   return loading ? (
     <Loader />
-  ) : (
+  ) : subLoading ? (
+    <Loader message="Loading submission" />
+  ) : submission ? (
     <RenderFeedback averages={averages} submission={submission} />
+  ) : (
+    <p>Could not find submission</p>
   );
 };
 
 interface IFeedbackContainerProps {
-  submission: Submissions.ISubItem;
+  submissionId: number;
 }
 
 export default FeedbackContainer;
